70edfbe585b97ba0d6c6a9ab0eeedccc07382550
761579 <anjalimehra728@gmail.com>
2023-06-20 19:08:12 -0500

    sementic elements

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import Logo from  '../Assets/Logo.svg'
+function Footer(){
+    return(
+        <>
+        <img src={Logo} className="App-logo" alt="logo" height="100px" width="100px" />
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+        </>
+    )
+}
+export default Footer;
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import Logo from "../Assets/Logo.svg"
+export default function Nav(){
+    return(
+        <>
+        <img src={Logo} className="App-logo" alt="logo" height="100px" width="100px" />
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+        </>
+        )
+    }
